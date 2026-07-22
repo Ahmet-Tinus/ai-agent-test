c9eba1e34b649fc2ea032c0af458e65fc58b83be
Ahmet-Tinus <ahmet.tosun@tinustech.com>
2026-07-22 12:47:38 +0300

    feat: implement list users endpoint

--- backend/app/routers/user_router.py
+++ backend/app/routers/user_router.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 from app.schemas.user_schema import UserCreate
-from app.services.user_service import create_user
+from app.services.user_service import create_user, list_users
+
+
 router = APIRouter()
+
 @router.post("/")
 async def create_user_endpoint(user: UserCreate):
     return await create_user(user)
+
+@router.get("/")
+async def list_users_endpoint():
+    return await list_users()
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -3,6 +3,7 @@ from app.database.connection import db
 from app.schemas.user_schema import UserCreate
 from app.utils.serializer import serialize_user
 
+
 async def create_user(user: UserCreate):
     users_collection = db["users"]
     user_data = user.model_dump()
@@ -17,3 +18,9 @@ async def create_user(user: UserCreate):
     created_user = await users_collection.find_one({"_id": result.inserted_id})
 
     return serialize_user(created_user)
+
+async def list_users():
+    users_collection = db["users"]
+    users_cursor = users_collection.find()
+    users = await users_cursor.to_list(length=None)
+    return [serialize_user(user) for user in users]
